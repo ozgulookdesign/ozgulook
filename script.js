a881383bceb43d966b69474a7e41b7dbd899110e
ozgulookdesign <three.marks@gmail.com>
2026-02-24 12:19:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+fetch('./products.json')
+    .then(response => response.json())
+    .then(data => {
+        const productList = document.getElementById('product-list');
+        data.forEach(product => {
+            const card = `
+                <div class="product-card">
+                    <a href="${product.link}">
+                        <img src="${product.image}" alt="${product.name}">
+                        <div class="product-name">${product.name}</div>
+                        <div class="product-price">${product.price}</div>
+                    </a>
+                </div>
+            `;
+            productList.innerHTML += card;
+        });
+    });
